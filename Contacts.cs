37e0572a18b6ce880bcaed2279bccb966a15bf82
poojakrp777 <poojakrp777@gmail.com>
2022-10-01 18:34:40 +0530

    Creating a contacts

---
+++ Contacts.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_Book
+{
+    internal class Contacts
+    {
+        public string First_name { get; set; }
+        public string Last_name { get; set; }
+
+        public string Address { get; set; }
+        public string State { get; set; }
+
+        public string City { get; set; }
+
+        public int Zip { get; set; }
+        public long Phone_number { get; set; }
+
+        public string Email { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_Book
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my Address Book");
+        }
+    }
+}
