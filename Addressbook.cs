8e6573d6faee696e9a4b4caf5bdd108dc4a642c0
poojakrp777 <poojakrp777@gmail.com>
2022-10-06 15:01:03 +0530

    Edit Contacts

--- Addressbook.cs
+++ Addressbook.cs
@@ -30,6 +30,46 @@ namespace Address_Book
             add.Add(con);
         }
 
+        public void addPerson()
+        {
+            Contacts newcontact = new Contacts();
+
+            Console.WriteLine("Enter First Name of Person :");
+            newcontact.First_Name = Console.ReadLine();
+
+            foreach (Contacts contact in add)
+            {
+                if (contact.First_Name == newcontact.First_Name)
+                {
+                    Console.WriteLine("Person with this Name Already Exists");
+                    return;
+                }
+            }
+            Console.WriteLine("2)Enter Last Name");
+            newcontact.Last_Name = Console.ReadLine();
+
+            Console.WriteLine("3)Enter Address");
+            newcontact.Address = Console.ReadLine();
+
+            Console.WriteLine("4)Enter City Name");
+            newcontact.City = Console.ReadLine();
+
+            Console.WriteLine("5)Enter State");
+            newcontact.State = Console.ReadLine();
+
+            Console.WriteLine("6)Enter Zip Code");
+            newcontact.Zip = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("7)Enter Phone Number");
+            newcontact.Phone_number = long.Parse(Console.ReadLine());
+
+            Console.WriteLine("8)Enter Email-Id");
+            newcontact.Email = Console.ReadLine();
+
+            add.Add(newcontact);
+        }
+
+
         public void Display()
         {
             foreach (Contacts data in add)
@@ -43,6 +83,56 @@ namespace Address_Book
                 Console.WriteLine("PhoneNumber:" + data.Phone_number);
 
             }
+        }
+
+        public void EditContact()
+        {
+            Console.WriteLine("To Edit Contact list Enter Contact First Name");
+            string name = Console.ReadLine().ToLower();
+            foreach (var data in add)
+            {
+                if (add.Contains(data))
+                {
+                    Console.WriteLine("To edit Contact Enter 1.LastName\n" +
+                        "2.Address\n 3.City\n 4.State\n 5.Zipcode\n 6.Phone Number\n");
+                    int options = Convert.ToInt32(Console.ReadLine());
+                    switch (options)
+                    {
+                        case 1:
+                            string lastname = Console.ReadLine();
+                            data.Last_Name = lastname;
+                            break;
+                        case 2:
+                            string address = Console.ReadLine();
+                            data.Address = address;
+                            break;
+                        case 3:
+                            string city = Console.ReadLine();
+                            data.City = city;
+                            break;
+                        case 4:
+                            string state = Console.ReadLine();
+                            data.State = state;
+                            break;
+                        case 5:
+                            int zipcode = int.Parse(Console.ReadLine());
+                            data.Zip = zipcode;
+                            break;
+                        case 6:
+                            long phonenumber = long.Parse(Console.ReadLine());
+                            data.Phone_number = phonenumber;
+                            break;
+                        default:
+                            Console.WriteLine("Enter Valid Option");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Name doesn't Matches");
+                }
+            }
+
         }
 
     }
--- Program.cs
+++ Program.cs
@@ -11,8 +11,86 @@ namespace Address_Book
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program");
-            AddressBookClass add = new AddressBookClass();
-            add.AddContact();
+
+            bool check = true;
+
+            while (check)
+            {
+                Console.WriteLine("Please select the Options :\n" +
+                    "1)Enter Details to add Contact in List\n" +
+                    "2)Add new Contact, Display and Stores contact in the list\n" +
+                    "3)Edit Details of Contact\n");
+
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+
+                        AddressBookClass addobj = new AddressBookClass();
+                        Console.WriteLine("--------------------");
+                        addobj.AddContact();
+                        break;
+
+                    case 2:
+
+                        AddressBookClass addobj1 = new AddressBookClass();
+                        Console.WriteLine("---------------------");
+                        addobj1.AddContact();
+                        Console.WriteLine("-----------------------------");
+                        Console.WriteLine("Entered Details of Person given as :");
+                        addobj1.Display();
+                        addobj1.addPerson();
+                        break;
+
+                    case 3:
+
+                        AddressBookClass addobj2 = new AddressBookClass();
+                        Console.WriteLine("------------------------");
+
+                        bool check1 = true;
+
+                        while (check1)
+                        {
+                            Console.WriteLine("Follow Steps to add details:\n" +
+                               "1) To addContact\n" + "2) To Edit Contact\n" +
+                               "3) To Display Contact\n");
+
+                            int select = Convert.ToInt32(Console.ReadLine());
+                            switch (select)
+                            {
+                                case 1:
+                                    addobj2.addPerson();
+                                    Console.WriteLine("--------------------------");
+                                    break;
+
+                                case 2:
+                                    addobj2.EditContact();
+                                    Console.WriteLine("------------------------");
+                                    break;
+
+                                case 3:
+                                    addobj2.Display();
+                                    Console.WriteLine("---------------------------");
+                                    break;
+
+                                default:
+
+                                    check = !check;
+                                    break;
+                            }
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("Please select proper option");
+                        break;
+                }
+                check = false;
+                Console.WriteLine("Try Again ");
+                break;
+            }
         }
+
     }
 }
